5c681813d91a7cd01881e3fd7275765503218900
Lioscro <kmin@caltech.edu>
2020-02-26 20:35:26 -0800

    move cleaning to utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import pandas as pd
 
 # Paths to text files
@@ -50,3 +51,16 @@ def load_test_data():
 def load_movies():
     """Load the movie info as a Pandas DataFrame."""
     return pd.read_csv(MOVIES_PATH, names=MOVIES_COLUMNS, delimiter='\t')
+
+def add_missing_ratings(df, movie_ids, user_id=None):
+    """"Add a single rating of 2.5 to all movies with no ratings in the
+    given dataframe. The rating is made by the provided user id or the next
+    user id."""
+    user_id = user_id if user_id is not None else np.max(df.user_id) + 1
+    missing = set(movie_ids) - set(df.movie_id.values)
+
+    df_add = pd.DataFrame(
+        [[user_id, movie_id, 2.5] for movie_id in missing],
+        columns=df.columns
+    )
+    return pd.concat((df, df_add))
